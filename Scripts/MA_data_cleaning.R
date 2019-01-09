eb2b9d3c446ae46a4a0f40709f6ca93017db6f2b
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2019-01-09 12:02:40 +0100

    Innovation extracted

--- Scripts/MA_data_cleaning.R
+++ Scripts/MA_data_cleaning.R
@@ -1,6 +1,6 @@
 brains.it <- read.csv("~/Desktop/Tesis/R/declines_brain/Raw_data/brains.it.csv")
 
-#List of species with brain weight and IT
+#List of species with brain weight and IT-----
 
 brains.it$Encephalization<-brains.it$Brain.Weight..mg./brains.it$IT
 brains.t<-subset(brains.it, subset = (is.na(brains.it$Encephalization)==FALSE))
@@ -9,11 +9,48 @@ spain.bees<-subset(brains.t, subset = (brains.t$Country=="Spain"))
 list.of.spanish.bees<-unique(spain.bees$Species)
 list.of.spanish.bees<-as.data.frame(list.of.spanish.bees)
 
+list.of.spanish.bees
+list.of.species
+
 getwd()
 setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/declines_brain/Raw_data")
-write.csv(list.of.spanish.bees, "list.of.spanish.bees.csv")
-write.csv(list.of.species, "list.of.species.csv")
+#write.csv(list.of.spanish.bees, "list.of.spanish.bees.csv")
+#write.csv(list.of.species, "list.of.species.csv")
 setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/declines_brain/")
+
+#List of innovators-----
+# load package
+install.packages("googlesheets")
+library(googlesheets)
+gs_ls("Traits")
+be <- gs_title("Traits")
+gs_ws_ls(be)
+Traits <- gs_read(ss=be, ws = "Sheet1", skip=0)
+Traits<- as.data.frame(Traits)
+View(Traits)
+Traits$species
+summary(as.factor(Traits$value), maxsum = 1000)
+list.of.innovators.f<-subset(Traits, subset = (Traits$value == "Brick" | 
+                                                   Traits$value == "Plastic" |
+                                                   Traits$value == "Small mammal house" |
+                                                   Traits$value == "Birdhouse" |
+                                                   Traits$value == "PVC" |
+                                                   Traits$value == "Table" |
+                                                   Traits$value == "Rockwool" |
+                                                   Traits$value == "Plastic table"|
+                                                   Traits$value == "Plastic straw" |
+                                                   Traits$value == "Polycarbonate" |
+                                                   Traits$value == "Paper" |
+                                                   Traits$value == "Metal" |
+                                                   Traits$value == "latch hole" |
+                                                   Traits$value == "Fluff" |
+                                                   Traits$value == "Dried paint" |
+                                                   Traits$value == "Cardboard"))
+
+list.of.innovators<-unique(list.of.innovators.f$species)
+list.of.innovators
+
+
 
 #Does Queen have bigger brains?------
 boxplot(brains.it$Brain.Weight..mg./brains.it$IT~brains.it$Sex, ylab="Brain/IT")
