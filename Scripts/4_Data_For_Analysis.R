1d0bff9c3dd56cfaeb501cce1416c50cd493b03b
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2022-02-21 19:44:30 +0100

    Quick checks about the data

--- Scripts/4_Data_For_Analysis.R
+++ Scripts/4_Data_For_Analysis.R
@@ -12,3 +12,14 @@ colnames(final_data) <- str_to_title(colnames(final_data))
 write.csv(final_data, file=gzfile("Data/final_data.csv.gz"), row.names=FALSE)
 #MA, this would read as follows:
 final_data <- data.frame(fread("Data/final_data.csv.gz"))
+
+#Some quick checks about the data
+#Check number of species
+nlevels(factor(final_data$Species_name))
+#Check species above 100 records
+all_above_100 <- final_data %>% 
+    group_by(Species_name) %>% filter(n() >= 100) %>% ungroup()
+nlevels(factor(all_above_100$Species_name))
+#Check number of records by continent
+final_data %>% filter(Continent=="Europe") #46701
+final_data %>% filter(Continent=="North America") #3239
