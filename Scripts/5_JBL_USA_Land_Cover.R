ae66dd5c991bb6a93c1684f557c35a143b2dd864
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-08-04 13:13:30 +0200

    preparing scripts for calculating preferences

---
+++ Scripts/5_JBL_USA_Land_Cover.R
@@ -0,0 +1,87 @@
+#Read clean USA data and extract land use from raster file
+#Load libraries
+library(data.table)
+library(tidyverse)
+
+#Load data
+usa <- read.table("Data/Usa_data/all_above_50_usa.csv.gz",  header=T, quote="\"", sep=",",row.names=1)
+#usa <- data.frame(fread("Data/Usa_data/all_above_50_usa.csv.gz"))
+
+#check levels, min 106 max just over 15.0000
+usa %>% 
+group_by(species) %>% 
+summarize(no_rows= length(species))
+
+
+#Extract land use
+#Code from using this code as an example https://www.r-bloggers.com/2014/11/spatial-data-extraction-around-buffered-points-in-r/
+names(usa)[which(names(usa)=="lat")]<-"latitude"
+names(usa)[which(names(usa)=="long")]<-"longitude"
+
+#year=2011
+#point_d = "Datos.csv" 
+#data_dir="NLCD_data"
+#write_dir="Data/Usa_data"
+
+extract_cover <- function(year,
+                          point_d = "Datos.csv",  
+                          data_dir="NLCD_data",
+                          write_dir="Data/Usa_data"){
+    require(raster)
+    require(rgdal)
+    require(stringr)
+    
+    # load cover data 
+    filename <- paste(data_dir, "/nlcd_",
+                      year,
+                      "_land_cover_l48_20210604.img",
+                      sep="")
+    NLCD <- raster(filename)
+    
+    # load site data
+    Datos <- read.csv(point_d, header=T, sep=",") #Si falla, usar como sep "," o ";"
+    #según tus datos
+    coords <- Datos[, c("longitude", "latitude")]  
+    #convert lat/lon to appropriate projection
+    names(coords) <- c("x", "y")
+    coordinates(coords) <- ~x + y
+    proj4string(coords) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+    crs_args <- NLCD@crs@projargs
+    Datos_transformed <- spTransform(coords, CRS(crs_args))
+    
+    #extract land cover data for each point, given buffer size
+    Landcover <- extract(NLCD, Datos_transformed)
+    
+    # summarize each site's data by proportion of each cover type
+    summ <- lapply(Landcover, function(x){
+        prop.table(table(x))
+    }
+    )
+    
+    # generate land cover number to name conversions
+    num.codes <- unique(unlist(Landcover))
+    cover.names <- NLCD@data@attributes[[1]]$NLCD.Land.Cover.Class[num.codes + 1]
+    levels(cover.names)[1] <- NA # first level is ""
+    conversions <- data.frame(num.codes, cover.names)
+    conversions <- na.omit(conversions)
+    conversions <- conversions[order(conversions$num.codes),]
+    
+    # convert to data frame
+    mydf <- data.frame(cover = names(unlist(summ)))
+    
+    mydf <- cbind(mydf,Datos)
+    
+    # create cover name column
+    mydf$num.codes <- mydf$cover
+    
+    mydf <- merge(conversions, mydf, by = "num.codes")
+    
+    # write output
+    out_name <- paste(write_dir, "/", "land_cover_", 
+                      "usa.csv", sep="")
+    write.csv(mydf, out_name)
+}
+
+#Extract cover with function
+extract_cover(year = 2011, point_d = "Datos.csv", write_dir = "Data/Usa_data")
+
---
+++ Scripts/6_JBL_USA_Preferences.R
@@ -0,0 +1,32 @@
+#Calculate habitat preferences
+
+#Load library
+library(tidyverse)
+
+#Load extracted data
+pref <- read_csv("Data/Usa_data/land_cover_usa.csv") %>% 
+dplyr::select(species, cover.names) %>% 
+mutate_if(is.character,as.factor)
+
+
+data_wide <- spread(pref, species, cover.names)
+
+pref.table = pref %>%
+    count(species, cover.names) %>%
+    pivot_wider(names_from = cover.names, values_from = n, values_fill = list(n = 0))
+
+
+
+pref.table<-aggregate(cover.names~species, FUN=summary, pref)
+
+
+#Convertimos long data en wide data, así sacamos cuantas ocurrencias de cada especie en cada habitat
+pref.table<-aggregate(pref$cover.names~pref$species, FUN=summary, pref)
+pref.table<-c(pref.table)
+pref.tabletemp<-pref.table$`pref$cover.names`
+rownames(pref.tabletemp)<-pref.table$`pref$species`
+usa_table<-data.frame(pref.tabletemp)
+str(usa_table)
+str(pref.table)
+names(pref.table)
+View(usa_table)
