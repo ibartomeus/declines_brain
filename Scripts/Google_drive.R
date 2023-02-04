f490e60be0134abd6fcb2b30b6754d071e12ab53
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2023-02-05 00:13:48 +0100

    Adding script to upload MS to google drive

---
+++ Scripts/Google_drive.R
@@ -0,0 +1,28 @@
+#Upload files to google drive
+#install packages
+remotes::install_github("claudiozandonella/trackdown",
+                        build_vignettes = TRUE, force=T)
+
+install.packages("googledrive")
+
+#set google doc
+googledrive::drive_auth()
+
+#load libraries
+library(googledrive)
+library(trackdown)
+
+#upload file
+
+#update_file
+update_file(
+  file = "Manuscript/Lanuza_et_al_2022.Rmd", 
+  gfile = "Reproductive_traits"
+)
+
+
+#download file 
+download_file(
+  file = "Manuscript/Lanuza_et_al_2022.Rmd", 
+  gfile = "Reproductive_traits"
+)
